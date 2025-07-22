d38fd5c15681ada1d7df02b4be168cc8465c5562
Basem Serag <basemserag.911@gmail.com>
2025-07-22 23:52:14 +0100

    updating  import paths in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 // fontAwesome
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Landing from './components/Layout/Landing/Landing';
 import About from './components/Layout/Pages/About/About';
-import Portfolio from './components/layout/Pages/Portfolio/Portfolio';
-import Contact from './components/layout/Pages/Contact/Contact';
+import Portfolio from './components/Layout/Pages/Portfolio/Portfolio';
+import Contact from './components/Layout/Pages/Contact/Contact';
 import Notfound from './components/Shared/NotFound/Notfound';
 
 function App() {
