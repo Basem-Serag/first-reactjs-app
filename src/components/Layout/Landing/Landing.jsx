0d4b3be61d4b719be08ecd2b4069da4da897996b
Basem Serag <basemserag.911@gmail.com>
2025-07-23 00:50:11 +0100

    [Enh]: adding new enhancements on landing section size

--- src/components/Layout/Landing/Landing.jsx
+++ src/components/Layout/Landing/Landing.jsx
@@ -4,7 +4,7 @@ import avatarImg from '../../../assets/imgs/avataaars.svg';
 export default function Landing() {
   return (
     <>
-      <section className="landing-section text-white text-center py-5 d-flex align-items-center">
+      <section className="landing-section vh-100 text-white text-center py-5 d-flex align-items-center">
         <div className="container">
           <div className="row justify-content-center py-5">
             <div className="col-sm-6 col-lg-3">
